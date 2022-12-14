3b6d90f06802b597c34df669c0976dd55462b4ed
Vlad <Smirnov.ru92@mail.ru>
2022-12-14 11:31:06 +0500

    HW8(1)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -14,7 +14,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
         }
         case 'check': {
 
-            return [...state.filter(el => el.age >= 18).reverse()] // need to fix
+            return [...state.sort((a, b) => a.name.localeCompare(b.name)).filter(el => el.age >= 18)] // need to fix
         }
         default:
             return state
