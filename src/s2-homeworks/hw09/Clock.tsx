ecbc661ba722d83595c51bd7e9de1dff0c6b00d3
Vlad <Smirnov.ru92@mail.ru>
2022-12-29 19:16:51 +0500

    HW9(1)

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -47,8 +47,8 @@ const stringTime = `${addNull(hours())}:${addNull(minutes())}:${addNull(seconds(
     const stringDate = `${date.toISOString().split("T")[0].replaceAll('-','.').split('.').reverse().join('.')}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const stringDay = date.getDay() || <br/> // пишут студенты
+    const stringMonth = date.getMonth() || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
